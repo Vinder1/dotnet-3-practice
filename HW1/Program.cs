82a405d35ffeabdef8485a475057db401344ae9c
VinderX <kar.galiullin@gmail.com>
2026-09-02 21:32:33 +0300

    hw1

---
+++ HW1/Program.cs
@@ -0,0 +1,15 @@
+﻿// task1
+var word = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(word))
+    word = "Привет";
+var reversedWord = word.Reverse().ToArray();
+Console.WriteLine(reversedWord);
+
+// task2
+var line = Console.ReadLine();
+if (line == null)
+    return;
+var words = line.Split();
+Console.WriteLine(string.Join(" ",
+    words.Select(w => new string(w.Reverse().ToArray()))
+));
